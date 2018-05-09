dcae5ae5f97cd75c358332bce7a438b20619531a
Nicolas DEL CASTILLO <ndelcast.dev@gmail.com>
2018-05-09 19:45:44 +0200

    FIX login

--- Modules/Architect/Http/Controllers/ArchitectController.php
+++ Modules/Architect/Http/Controllers/ArchitectController.php
@@ -6,10 +6,21 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Controller;
 
+use Illuminate\Support\Facades\Auth;
+
 class ArchitectController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
+
+        print_R(Auth::user());
+
         return view('architect::index');
     }
 }
