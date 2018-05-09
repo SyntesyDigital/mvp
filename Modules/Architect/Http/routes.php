30a81627e901bef6e6f9fa8331baf72804958ad2
Nicolas DEL CASTILLO <ndelcast.dev@gmail.com>
2018-05-09 19:47:20 +0200

    FIXING login

--- Modules/Architect/Http/routes.php
+++ Modules/Architect/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'web', 'prefix' => 'architect', 'namespace' => 'Modules\Architect\Http\Controllers'], function()
+Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'architect', 'namespace' => 'Modules\Architect\Http\Controllers'], function()
 {
     Route::get('/', 'ArchitectController@index');
 });
