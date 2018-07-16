8000c92995140e0366669eedebd468cb1ad5dbcc
Nicolas DEL CASTILLO <ndelcast.dev@gmail.com>
2018-07-16 13:55:32 +0100

    FIX

--- Modules/Architect/Widgets/Types/WidgetList1.php
+++ Modules/Architect/Widgets/Types/WidgetList1.php
@@ -14,28 +14,29 @@ class WidgetList1 extends Widget implements WidgetInterface
     public $type = 'widget-list';
     public $icon = 'fa-font';
     public $name = 'WIDGET_LIST_1';
+
     public $component = 'CommonWidget';
 
-    public $fields = [
-        [
-            "class" => 'Modules\Architect\Fields\Types\Text',
-            "identifier" => "title",
-            "type" => "text", // <= FIXME : ex : Text::getType()
-            "name" => "Títol", // <= FIXME : translate it!
-        ],
-        [
-            "class" => 'Modules\Architect\Fields\Types\Text',
-            "identifier" => "slug",
-            "type" => "text", // <= FIXME
-            "name" => "Slug" // <= FIXME : translate it!
-        ],
-        [
-            "class" => 'Modules\Architect\Fields\Types\Link',
-            "identifier" => "link",
-            "type" => "link", // <= FIXME
-            "name" => "Link" // <= FIXME : translate it!
-        ],
-    ];
+    // public $fields = [
+    //     [
+    //         "class" => 'Modules\Architect\Fields\Types\Text',
+    //         "identifier" => "title",
+    //         "type" => "text", // <= FIXME : ex : Text::getType()
+    //         "name" => "Títol", // <= FIXME : translate it!
+    //     ],
+    //     [
+    //         "class" => 'Modules\Architect\Fields\Types\Text',
+    //         "identifier" => "slug",
+    //         "type" => "text", // <= FIXME
+    //         "name" => "Slug" // <= FIXME : translate it!
+    //     ],
+    //     [
+    //         "class" => 'Modules\Architect\Fields\Types\Link',
+    //         "identifier" => "link",
+    //         "type" => "link", // <= FIXME
+    //         "name" => "Link" // <= FIXME : translate it!
+    //     ],
+    // ];
 
     public $rules = [
         'required'
