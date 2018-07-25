879e3f8353cb3c14742dde22455d7cf45d701eec
Sergi Mínguez Regidor <sergi.minguez@cocreativa.com>
2018-07-25 17:20:09 +0200

    contents

--- Modules/Turisme/Resources/views/partials/typologies/producte.blade.php
+++ Modules/Turisme/Resources/views/partials/typologies/producte.blade.php
@@ -2,5 +2,4 @@
   <p class="image"><img src="images/img-medium.png"  alt=""/></p>
   <p class="titol">Lorem ipsum dolor sit amet</p>
   <p class="text">19-01-2017</p>
-  <p class="text"><a href="" >Català</a>| <a href="" >Español</a>| <a href="" >English</a>| <a href="" >Français</a>| <a href="" >русский</a>| <a href="" >中国</a>| </p>
 </div>
