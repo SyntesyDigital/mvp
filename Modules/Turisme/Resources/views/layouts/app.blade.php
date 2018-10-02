af1b5c92f8d610b785ef0561eb3e6116955f5ffa
Sergi Mínguez Regidor <sergi.minguez@cocreativa.com>
2018-10-02 12:30:44 +0200

    remove css

--- Modules/Turisme/Resources/views/layouts/app.blade.php
+++ Modules/Turisme/Resources/views/layouts/app.blade.php
@@ -34,7 +34,9 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
 
+        <!--
         <link href="{{asset('modules/turisme/css/turisme.css')}}" rel="stylesheet" type="text/css" />
+        -->
 
         @stack('styles')
 
