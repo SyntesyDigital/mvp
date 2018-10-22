d78d602ca7982a30eb91bebeb6768d99101006c1
DANI <dani@example.com>
2018-10-22 16:42:39 +0200

    rotatory changes

--- Modules/Turisme/Resources/views/partials/widgets/thumb_slide.blade.php
+++ Modules/Turisme/Resources/views/partials/widgets/thumb_slide.blade.php
@@ -7,25 +7,27 @@
       "class" => 'center-block'
     ]
   )
+  @if(isset($field['fields'][1]['value']['title'][App::getLocale()]) && $field['fields'][1]['value']['title'][App::getLocale()] != '')
 
-  <div class="carousel-caption">
-	  <p>
-      @include('turisme::partials.fields.'.$field['fields'][1]['type'],
-        [
-          "field" => $field['fields'][1],
-          "settings" => $settings,
-          "div" => false
-        ]
-      )
-    </p>
-    <p class="subtitle">
-      @include('turisme::partials.fields.'.$field['fields'][2]['type'],
-        [
-          "field" => $field['fields'][2],
-          "settings" => $settings,
-          "div" => false
-        ]
-      )
-    </p>
-  </div>
+    <div class="carousel-caption">
+  	  <p>
+        @include('turisme::partials.fields.'.$field['fields'][1]['type'],
+          [
+            "field" => $field['fields'][1],
+            "settings" => $settings,
+            "div" => false
+          ]
+        )
+      </p>
+      <p class="subtitle">
+        @include('turisme::partials.fields.'.$field['fields'][2]['type'],
+          [
+            "field" => $field['fields'][2],
+            "settings" => $settings,
+            "div" => false
+          ]
+        )
+      </p>
+    </div>
+  @endif
 </div>
