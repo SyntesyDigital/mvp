5040d1b37934320dc134c279703261b3773b9857
Sergi Mínguez Regidor <sergi.minguez@cocreativa.com>
2018-12-13 12:37:51 +0100

    offer-nav update

--- Modules/RRHH/Resources/views/admin/partials/offers-nav.blade.php
+++ Modules/RRHH/Resources/views/admin/partials/offers-nav.blade.php
@@ -1,40 +1,43 @@
 <div class="sidebar">
-
   <ul>
         @if(isset($typology_id) && $typology_id == $typology->id)
           <li class="active">
         @else
           <li>
         @endif
-          <a href="{{route('rrhh.admin.offers.index')}}"><i class="fa fa-pages"></i><span class="text">Liste des offres</span> </a>
+          <a href="{{route('rrhh.admin.offers.index')}}">
+            <i class="fa fa-file"></i><span class="text">Offres</span> </a>
         </li>
 
         <li>
-          <a href="{{route('rrhh.admin.applications.index')}}"><i class="fa fa-pages"></i><span class="text">Liste des candidatures</span> </a>
+          <a href="{{route('rrhh.admin.applications.index')}}">
+            <i class="fa fa-address-card"></i><span class="text">Candidatures</span> </a>
         </li>
 
         <li>
-          <a href="{{route('rrhh.admin.applications.spontaneous')}}"><i class="fa fa-pages"></i><span class="text">Liste des candidatures spontanées</span> </a>
+          <a href="{{route('rrhh.admin.applications.spontaneous')}}">
+            <i class="fa fa-address-card-o"></i><span class="text">C. spontanées</span> </a>
         </li>
    </ul>
-
    <hr />
-
    <ul>
         <li>
-          <a href="{{route('rrhh.admin.candidates.index')}}"><i class="fa fa-pages"></i><span class="text">Liste des candidats</span> </a>
+          <a href="{{route('rrhh.admin.candidates.index')}}">
+            <i class="fa fa-user"></i><span class="text">Candidats</span> </a>
         </li>
 
         <li>
-          <a href="{{route('rrhh.admin.customers.index')}}"><i class="fa fa-pages"></i><span class="text">Liste des clients</span> </a>
+          <!--
+          <a href="{{route('rrhh.admin.customers.index')}}">
+            <i class="fa fa-user-o"></i><span class="text">Clients</span>
+          </a>
+          -->
         </li>
     </ul>
-
     <hr/>
-
     <ul>
         <li>
-          <a href="{{route('rrhh.admin.tags.index')}}"><i class="fa fa-pages"></i><span class="text">Liste des tags</span> </a>
+          <a href="{{route('rrhh.admin.tags.index')}}"><i class="fa fa-tag"></i><span class="text">Tags</span> </a>
         </li>
     </ul>
     <hr/>
