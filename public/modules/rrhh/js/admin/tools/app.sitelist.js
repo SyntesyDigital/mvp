f1463ac7db65cde2f4b7b593d117c7663cc5e536
DANI <dani@example.com>
2018-12-28 17:29:47 +0100

    sitelists

--- public/modules/rrhh/js/admin/tools/app.sitelist.js
+++ public/modules/rrhh/js/admin/tools/app.sitelist.js
@@ -23,26 +23,16 @@ app.sitelist = {
         var node = "<div class='row' id='options-ele'>";
         for (var i = 0; i < list_json.length; i++) {
             var ele = list_json[i];
-            node +=
-
-
-
-            "<div class=\"typology-field dragable-option\" draggable=\"true\" ondragstart=\"app.sitelist.dragstart(event)\"  ondragenter=\"app.sitelist.dragenter(event)\" style=\"cursor: move; opacity: 1;\"> "+
-              "<div class=\"field-type content-add \"><i class=\"fa fa-reorder\"></i> &nbsp; Element</div>"+
-              "<div class=\"field-inputs content-add\">"+
-                "<div class=\"row\">"+
-                  "<div class=\"field-name col-xs-6\">"+
-                    "<div class=\"form-group\"><input type=\"text\" class=\"form-control input-name-op\" name=\"name_json\" oninput=\"list_json[" + i + "].name = this.value;app.sitelist.updatePreview();\" placeholder=\"name\" value=\"" + ele.name + "\"><span class=\"material-input\"></span></div>"+
-                  "</div>"+
-                  "<div class=\"field-id col-xs-6\">"+
-                    "<div class=\"form-group\"><input type=\"text\" class=\"form-control input-value-op\" name=\"value_json\" oninput=\"list_json[" + i + "].value = this.value;app.sitelist.updatePreview();\" placeholder=\"value\" value=\"" + ele.value + "\"><span class=\"material-input\"></span></div>"+
-                  "</div>"+
-                "</div>"+
-              "</div>"+
-              "<div class=\"field-actions content-add\"><a href=\"#\" onclick=\"app.sitelist.removeContent(" + i + ")\" class=\"remove-field-btn\"> <i class=\"fa fa-trash\"></i> Supprimer </a>&nbsp;&nbsp;</div>"+
-            "</div>";
+            node += "<div class=\"col-md-12 dragable-option\" draggable=\"true\" ondragstart=\"app.sitelist.dragstart(event)\"  ondragenter=\"app.sitelist.dragenter(event)\" >" +
+                "<div class=\"content-add\">" +
+                "<div class=\"tool-box\">" +
+                "<a class=\"remove-action\" onclick=\"app.sitelist.removeContent(" + i + ")\"><i class=\"fa fa-remove\"></i></a>" +
+                "</div>" +
+                "<input type=\"text\" name=\"name_json\" oninput=\"list_json[" + i + "].name = this.value;app.sitelist.updatePreview();\" class=\"form-control input-lg input-name-option\" placeholder=\"name\" value=\"" + ele.name + "\"  />" +
+                "<input type=\"text\" name=\"value_json\" oninput=\"list_json[" + i + "].value = this.value;app.sitelist.updatePreview();\" class=\"form-control input-sm input-value-option\" placeholder=\"value\" value=\"" + ele.value + "\"  />" +
+                "</div>" +
+                "</div>";
         }
-        node +=   "<div class=\"ajouter-element dashed-border\"><a href=\"#\" class=\"btn btn-default\" id=\"ajouter\" onclick=\"app.sitelist.addNewElement()\"><i class=\"fa fa-plus\"></i> AJOUTER</a></div>";
         node += "</div>"
         $('.content-field-dropper').html(node);
         this.updatePreview();
@@ -76,8 +66,12 @@ app.sitelist = {
         if (source.nodeName == 'INPUT') {
             return false
         } else {
-            if (targetelem.getAttribute('class') != "typology-field dragable-option") {
-                targetelem = targetelem.closest('.dragable-option');
+            if (targetelem.nodeName == "INPUT") {
+                targetelem = targetelem.parentNode.parentNode;
+            } else {
+                if (targetelem.getAttribute('class') == 'content-add') {
+                    targetelem = targetelem.parentNode;
+                }
             }
             if (this.isbefore(source, targetelem)) {
                 targetelem.parentNode.insertBefore(source, targetelem);
@@ -93,34 +87,15 @@ app.sitelist = {
         list_json = [];
         for (var i = 0; i < options.length; i++) {
             list_json.push({
-                name: options[i].getElementsByClassName("input-name-op")[0].value,
-                value: options[i].getElementsByClassName("input-value-op")[0].value
+                name: options[i].getElementsByClassName("input-name-option")[0].value,
+                value: options[i].getElementsByClassName("input-value-option")[0].value
             });
         }
         this.parseJson();
     },
     removeContent: function(i) {
-      event.preventDefault();
-      var self = this;
-      bootbox.confirm({
-          message: 'Etes-vous sur de vouloir supprimer cette content ?',
-          buttons: {
-              confirm: {
-                  label: 'Oui',
-                  className: 'btn-primary'
-              },
-              cancel: {
-                  label: 'Non',
-                  className: 'btn-default'
-              }
-          },
-          callback: function(result) {
-              if (result) {
-                list_json.splice(i, 1);
-                self.parseJson();
-              }
-          }
-      });
+        list_json.splice(i, 1);
+        this.parseJson();
     },
     updatePreview: function() {
         var type = $('#type').val(),
@@ -175,26 +150,26 @@ app.sitelist = {
 
 }
 
-$('#general-delete-btn').on('click', function(e) {
-  e.preventDefault();
-  bootbox.confirm({
-      message: 'Etes-vous sur de vouloir supprimer cette List ?',
-      buttons: {
-          confirm: {
-              label: 'Oui',
-              className: 'btn-primary'
-          },
-          cancel: {
-              label: 'Non',
-              className: 'btn-default'
-          }
-      },
-      callback: function(result) {
-          if (result) {
-              $('form.delete-sitelist-form')
-                  .off('submit')
-                  .trigger('submit');
-          }
-      }
-  });
+$('form.delete-sitelist-form').on('submit', function(e) {
+    e.preventDefault();
+    bootbox.confirm({
+        message: 'Etes-vous sur de vouloir supprimer cette List ?',
+        buttons: {
+            confirm: {
+                label: 'Oui',
+                className: 'btn-success'
+            },
+            cancel: {
+                label: 'Non',
+                className: 'btn-danger'
+            }
+        },
+        callback: function(result) {
+            if (result) {
+                $('form.delete-sitelist-form')
+                    .off('submit')
+                    .trigger('submit');
+            }
+        }
+    });
 });
