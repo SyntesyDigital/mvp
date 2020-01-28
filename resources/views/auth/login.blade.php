68d0959518a184512c8193c9c048a098d7066360
Dani <dani@MacBook-Air-de-Dani.local>
2020-01-28 11:09:07 +0100

    login styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,49 +5,20 @@
 @section('content')
 <div class="container login-container">
     <div class="row justify-content-center">
-        <div class="col-md-7 icons">
-          <div class="col-md-4 icon-box">
-            <div class="icon">
-              <i class="fas fa-lock"></i>
-            </div>
-            <div class="title">
-              Espace client
-            </div>
-            <div class="separator"></div>
-            <p>En un clic, accédez à votre dossier</p>
-          </div>
-          <div class="col-md-4 icon-box">
-            <div class="icon">
-              <i class="fas fa-phone"></i>
-            </div>
-            <div class="title">
-              Web call back
-            </div>
-            <div class="separator"></div>
-            <p>En un clic, accédez à votre dossier</p>
-          </div>
-          <div class="col-md-4 icon-box">
-            <div class="icon">
-              <i class="fas fa-laptop"></i>
-            </div>
-            <div class="title">
-              Déclaration en ligne
-            </div>
-            <div class="separator"></div>
-            <p>En un clic, accédez à votre dossier</p>
-          </div>
-          <div class="separator"></div>
-          <p>Nous mettons à la disposition de nos clients de nombreux outils numériques permettant de faciliter leurs démarches</p>
-
-        </div>
-
-        <div class="col-md-5 login-box-container">
+        <div class="login-box-container">
           <div class="login-box">
             <form method="POST" action="{{ route('login') }}">
               @csrf
-              <h2>Connexion</h2>
+              <div class="logo-container">
+                @if(isset($storedStylesFront['frontLogo']) && isset($storedStylesFront['frontLogo']->value))
+                  <img src="/{{$storedStylesFront['frontLogo']->value->urls['original']}}" alt="Logo" />
+                @else
+                  <img src="{{asset('modules/architect/images/logo.png')}}" alt=""/>
+                @endif
+              </div>
+              <h2>Connectez-vous</h2>
               <div class="form-group row">
-                  <label for="email" class="col-sm-12 col-form-label text-md-right">Utilisateur</label>
+                  <label for="email" class="col-sm-12 col-form-label text-md-right"><i class="fa fa-user"></i>Utilisateur</label>
 
                   <div class="col-md-12">
                       <input id="uid" type="text" class="form-control{{ $errors->has('uid') ? ' is-invalid' : '' }}" name="uid" value="{{ old('uid') }}" placeholder="" required autofocus>
@@ -61,7 +32,7 @@
               </div>
 
               <div class="form-group row">
-                  <label for="passwd" class="col-md-12 col-form-label text-md-right">Mot de passe</label>
+                  <label for="passwd" class="col-md-12 col-form-label text-md-right"><i class="fa fa-lock"></i>Mot de passe</label>
 
                   <div class="col-md-12">
                       <input id="passwd" type="password" class="form-control{{ $errors->has('passwd') ? ' is-invalid' : '' }}" name="passwd"  placeholder="" required>
